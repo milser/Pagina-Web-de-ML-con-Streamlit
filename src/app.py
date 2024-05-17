9fec64f1c50c29de899009ac3820ee774cda7025
milser <ruben2work@gmail.com>
2024-05-17 20:10:20 +0200

    c13

--- src/app.py
+++ src/app.py
@@ -63,53 +63,56 @@ similarity = joblib.load(r'C:\Users\milser\Documents\Trasteo_4geeks\Pagina-Web-d
 genres = film_data.genres.values
 crews = film_data.crew.values
 
-# Filtrar la lista de géneros para eliminar elementos que no son cadenas de texto
-genres = [genre for genre in genres if isinstance(genre, str)]
-unique_genres = set()
-for item in genres:
-    unique_genres.update(item.split())
-unique_genres_list = list(unique_genres)
-unique_genres_list.sort()
-
-
-selected_genre = st.multiselect("Selecciona un género:", unique_genres_list)
-st.text('Esto filtrara las peliculas disponibles en la lista inferior')
-
-def contiene_todos_los_generos(row, selected_genres):
-    # Dividir la cadena de géneros en una lista de géneros
-    if isinstance(row['genres'], str):
-        lista_de_generos = row['genres'].split()
-        # Verificar si todos los géneros seleccionados están presentes en la lista de géneros
-        for genre in selected_genres:
-            if genre not in lista_de_generos:
-                return False
-        return True
-    else:
-        return False 
-
-# Aplicar la función a cada fila del DataFrame
-film_data['cumple_condicion'] = film_data.apply(lambda row: contiene_todos_los_generos(row, selected_genre), axis=1)
-peliculas_filtradas = film_data[film_data['cumple_condicion']]
-peliculas_filtradas = peliculas_filtradas.drop(columns=['cumple_condicion'])
-
-selected_film = st.selectbox("Indica una pelicula que te guste como orientación:", peliculas_filtradas.title.values)
-
-
 def recommend(movie):
-    recomended_list = []
-    try:    
-    
-        movie_index = film_data[film_data["title"] == movie].index[0]
-        distances = similarity[movie_index]
-        movie_list = sorted(list(enumerate(distances)), reverse = True , key = lambda x: x[1])[1:nFilmsRecomended+1]
-    
-        for i in movie_list:
-            print(film_data.iloc[i[0]].title)
-            recomended_list.append(film_data.iloc[i[0]].title)
-        return recomended_list
-    except Exception as e:
-        print("Your movie is not in the List")
+        recomended_list = []
+        try:    
+        
+            movie_index = film_data[film_data["title"] == movie].index[0]
+            distances = similarity[movie_index]
+            movie_list = sorted(list(enumerate(distances)), reverse = True , key = lambda x: x[1])[1:nFilmsRecomended+1]
+        
+            for i in movie_list:
+                print(film_data.iloc[i[0]].title)
+                recomended_list.append(film_data.iloc[i[0]].title)
+            return recomended_list
+        except Exception as e:
+            print("Your movie is not in the List")
+
+def main():
+    # Filtrar la lista de géneros para eliminar elementos que no son cadenas de texto
+    genres = [genre for genre in genres if isinstance(genre, str)]
+    unique_genres = set()
+    for item in genres:
+        unique_genres.update(item.split())
+    unique_genres_list = list(unique_genres)
+    unique_genres_list.sort()
 
 
-for string in recommend(selected_film):
-    st.markdown(f"### {string}")
+    selected_genre = st.multiselect("Selecciona un género:", unique_genres_list)
+    st.text('Esto filtrara las peliculas disponibles en la lista inferior')
+
+    def contiene_todos_los_generos(row, selected_genres):
+        # Dividir la cadena de géneros en una lista de géneros
+        if isinstance(row['genres'], str):
+            lista_de_generos = row['genres'].split()
+            # Verificar si todos los géneros seleccionados están presentes en la lista de géneros
+            for genre in selected_genres:
+                if genre not in lista_de_generos:
+                    return False
+            return True
+        else:
+            return False 
+
+    # Aplicar la función a cada fila del DataFrame
+    film_data['cumple_condicion'] = film_data.apply(lambda row: contiene_todos_los_generos(row, selected_genre), axis=1)
+    peliculas_filtradas = film_data[film_data['cumple_condicion']]
+    peliculas_filtradas = peliculas_filtradas.drop(columns=['cumple_condicion'])
+
+    selected_film = st.selectbox("Indica una pelicula que te guste como orientación:", peliculas_filtradas.title.values)
+
+    for string in recommend(selected_film):
+        st.markdown(f"### {string}")
+        
+        
+if __name__ == '__main__':
+    main()
